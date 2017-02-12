facf5a5156f9e3ca8a56677b4bc335206fd02743
MrGordoMaster <normanbg98@gmail.com>
2017-02-12 18:03:23 +0100

    Events 2.0

--- rpg 2/events.cpp
+++ rpg 2/events.cpp
@@ -2,6 +2,8 @@
 
 int events()
 {
-
+	int solution1;
+	printf("It is time for some riddles.\n First one: You feed it, it lives, you give it something to drink, it dies. What is?");
+	scanf(%s,solution1 );
 
 }
