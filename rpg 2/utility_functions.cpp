dfbe33451d30de420eaae3da301d63d1c3170497
Adria-F <adriaferrer2898@gmail.com>
2017-02-13 16:37:05 +0100

    just ignore v.2

--- rpg 2/utility_functions.cpp
+++ rpg 2/utility_functions.cpp
@@ -26,6 +26,16 @@ char* get_string()
 	return charP;
 }
 
+int char_to_int(char str[])
+{
+	int num;
+	num += (str[2] - 48);
+	num += (str[1] - 48) * 10;
+	num += (str[0] - 48) * 100;
+
+	return num;
+}
+
 void save_game()
 {
 
@@ -67,16 +77,22 @@ void check_and_load_game(FILE* game, hero_data* hero)
 			hero[0].name = text;
 			char saved_hp[3];
 			fgets(saved_hp, 4, game);
+			hero[0].hp = char_to_int(saved_hp);
 			char saved_attack[3];
 			fgets(saved_attack, 4, game);
+			hero[0].attack = char_to_int(saved_attck);
 			char saved_armor[3];
 			fgets(saved_armor, 4, game);
+			hero[0].armor = char_to_int(saved_armor);
 			char saved_lvl[3];
 			fgets(saved_lvl, 4, game);
+			hero[0].lvl = char_to_int(saved_lvl);
 			char saved_exp[3];
 			fgets(saved_exp, 4, game);
+			hero[0].exp = char_to_int(saved_exp);
 			char saved_coins[3];
 			fgets(saved_coins, 4, game);
+			hero[0].coins = char_to_int(saved_coins);
 		}
 	}
 }
